ce7550a8cadc3d26ce3ed5528cb6def8370ac907
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-05-10 19:24:41 +0200

    added sections to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { Header } from "./Header/header";
+import { Nav } from "./Nav/nav";
+
+import { Footer } from "./Footer/footer";
+import { Main } from "./Main/main";
+
 function App() {
   return (
-   <Header 
-    title={"Bankietowy Zakątek"}
-   />
+    <>
+      <Header
+        title={"Bankietowy Zakątek"}
+      />
+      <Nav />
+
+      <Main />
+      
+      <Footer
+        content={"strona stworzona w React.js przez Konrad Tusiński"}
+      />
+    </>
+
   );
+
 }
 
 export default App;
