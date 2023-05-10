a372b690080c0cbbe3bb0936a3b4159a100e9ee0
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-05-10 19:22:09 +0200

    nav changed

--- src/Nav/nav.js
+++ src/Nav/nav.js
@@ -6,6 +6,7 @@ export const Nav = () => (
             <li className="list__item">Oferty</li>
             <li className="list__item">Galeria</li>
             <li className="list__item">Kontakt</li>
+            <li className="list__item">Katering</li>
             <li className="list__item">O Nas</li>
         </ul>
     </nav>
