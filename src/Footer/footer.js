56462ad7e371d089a4d5685254d06c2b2a27eec7
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-05-10 19:21:51 +0200

    Footer added

---
+++ src/Footer/footer.js
@@ -0,0 +1,10 @@
+import "./style.css"
+
+export const Footer = ({ content }) => (
+    <footer className="footer">
+        <p className="footer__paragraph">
+            {content}
+        </p>
+    </footer>
+
+)
