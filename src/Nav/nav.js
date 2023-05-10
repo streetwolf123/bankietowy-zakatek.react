6339f44632729dd422a9684761326a06bcdaf8c0
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-05-10 16:37:48 +0200

    Nav added

---
+++ src/Nav/nav.js
@@ -0,0 +1,12 @@
+import "./style.css"
+
+export const Nav = () => (
+    <nav className="navigation">
+        <ul className="navigation__list">
+            <li className="list__item">Oferty</li>
+            <li className="list__item">Galeria</li>
+            <li className="list__item">Kontakt</li>
+            <li className="list__item">O Nas</li>
+        </ul>
+    </nav>
+)
